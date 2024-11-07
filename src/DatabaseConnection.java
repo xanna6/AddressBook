37747277d8ceb7f414b88e0406e33dab77c51921
xanna6 <apiotrow96@gmail.com>
2024-11-07 19:48:09 +0100

    searching for contacts based on textfield content

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -13,22 +13,49 @@ public class DatabaseConnection {
 
     public List<Contact> getContacts() {
         List<Contact> contacts = new ArrayList<>();
-
         String query = "SELECT first_name, last_name, phone, email FROM contact";
         try (Connection connection = connect();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
 
-            while (resultSet.next()) {
-                String firstName = resultSet.getString("first_name");
-                String lastName = resultSet.getString("last_name");
-                String phone = resultSet.getString("phone");
-                String email = resultSet.getString("email");
-
-                contacts.add(new Contact(firstName, lastName, phone, email));
-            }
+            contacts = mapResultSetToContactList(resultSet);
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+        return contacts;
+    }
+
+    public List<Contact> getFilteredContacts(String searchText) {
+        List<Contact> contacts = new ArrayList<>();
+        String query = "SELECT first_name, last_name, phone, email FROM contact WHERE first_name LIKE ? OR last_name LIKE ? OR phone LIKE ? OR email LIKE ?";
+        String searchPattern = "%" + searchText + "%";
+        try (Connection connection = connect();
+             PreparedStatement statement = connection.prepareStatement(query);) {
+            statement.setString(1, searchPattern);
+            statement.setString(2, searchPattern);
+            statement.setString(3, searchPattern);
+            statement.setString(4, searchPattern);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                contacts = mapResultSetToContactList(resultSet);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return contacts;
+    }
+
+    private List<Contact> mapResultSetToContactList(ResultSet resultSet) throws SQLException {
+        List<Contact> contacts = new ArrayList<>();
+        while (resultSet.next()) {
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String phone = resultSet.getString("phone");
+            String email = resultSet.getString("email");
+
+            contacts.add(new Contact(firstName, lastName, phone, email));
         }
         return contacts;
     }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -9,22 +9,51 @@ public class MainWindow  extends JFrame {
     public MainWindow() {
         setTitle("AddressBook");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JLabel label = new JLabel("Contact List", JLabel.CENTER);
+        add(label, BorderLayout.NORTH);
+
+        JPanel searchPanel = new JPanel(new BorderLayout(10,0));
+        JTextField searchField = new JTextField();
+        JButton searchButton = new JButton("Search");
+        searchButton.setToolTipText("Search");
+        searchButton.addActionListener(e -> {
+            String searchText = searchField.getText();
+            loadFilteredDataToContactTable(searchText);
+        });
+
+        searchPanel.add(searchField, BorderLayout.CENTER);
+        searchPanel.add(searchButton, BorderLayout.EAST);
+
+        JPanel searchContainer = new JPanel(new FlowLayout(FlowLayout.CENTER, 0,10));
+        searchContainer.add(searchPanel);
+        searchPanel.setPreferredSize(new Dimension(300, 20));
 
         contactTable = new JTable();
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        JLabel label = new JLabel("Contact List", JLabel.CENTER);
-        panel.add(label, BorderLayout.NORTH);
-
         dbConnection = new DatabaseConnection();
         loadDataToContactTable();
-        panel.add(new JScrollPane(contactTable), BorderLayout.CENTER);
-        add(panel);
+        JScrollPane tableScrollPane = new JScrollPane(contactTable);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.add(searchContainer);
+        mainPanel.add(tableScrollPane);
+
+        add(mainPanel, BorderLayout.CENTER);
 
         pack();
         setSize(600, 300);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void loadFilteredDataToContactTable(String searchText) {
+        List<Contact> filteredContacts = dbConnection.getFilteredContacts(searchText);
+        ContactTableModel model = new ContactTableModel(filteredContacts);
+
+        contactTable.setModel(model);
+
     }
 
     private void loadDataToContactTable() {
